d38ee979aade323b0102078f4fad2e86e3da1b80
Josep Cumplido <josepcumplido@gmail.com>
2024-11-24 21:43:27 +0100

    Profile page

--- client/my-app/app/profile/layout.tsx
+++ client/my-app/app/profile/layout.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-const LoginLayout = ({ children }: { children: React.ReactNode }) => {
+const LoginLayout = ({children}: { children: React.ReactNode }) => {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <div className="w-full max-w-md p-8 space-y-4 bg-white rounded-md shadow-md">
-                {children}
-            </div>
-        </div>
+        <>
+            {children}
+        </>
     );
 };
 
--- client/my-app/app/profile/page.tsx
+++ client/my-app/app/profile/page.tsx
@@ -1,11 +1,24 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
-import { useAuth } from '@/hooks/useAuth';
+import {useState, useEffect} from 'react';
+import {useRouter} from 'next/navigation';
+import {useAuth} from '@/hooks/useAuth';
 import jwt from 'jsonwebtoken'; // Asegúrate de tener 'jsonwebtoken' instalado
+import {Button} from "@/components/ui/button"
+import {Textarea} from "@/components/ui/textarea"
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import PasswordInput from "@/components/ui/password-input";
 
 interface UserInfo {
     username: string;
@@ -14,7 +27,7 @@ interface UserInfo {
 }
 
 const ProfilePage = () => {
-    const { isAuthenticated } = useAuth(); // Hook que verifica si el usuario está autenticado
+    const {isAuthenticated} = useAuth(); // Hook que verifica si el usuario está autenticado
     const [userInfo, setUserInfo] = useState<UserInfo | null>(null); // Definir el tipo de userInfo
     const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
     const [username, setUsername] = useState<string | null>(null);
@@ -66,9 +79,9 @@ const ProfilePage = () => {
         return <div>Loading...</div>; // Muestra un loader mientras se carga la información
     }
 
-    return (
+    /*return (
         <div className="flex flex-col items-center justify-center min-h-screen p-6 space-y-6">
-            {/* Avatar grande */}
+            {/!* Avatar grande *!/}
             <div>
                 <Avatar className="w-32 h-32">
                     <AvatarImage src={avatarUrl || "default-avatar.png"} alt="User Avatar"/>
@@ -76,28 +89,109 @@ const ProfilePage = () => {
                 </Avatar>
             </div>
 
-            {/* Username */}
+            {/!* Username *!/}
             <h1 className="text-3xl font-semibold">{userInfo.username}</h1>
 
-            {/* Botones de acción */}
+            {/!* Botones de acción *!/}
             <div className="flex flex-col items-center space-y-4">
 
                 <Button variant="outline" onClick={handleChats} className="w-40">
                     Xats
                 </Button>
 
-                {/* Botón de Editar Perfil */}
+                {/!* Botón de Editar Perfil *!/}
                 <Button variant="outline" onClick={handleEditProfile} className="w-40">
                     Editar Perfil
                 </Button>
 
-                {/* Botón de Logout */}
+                {/!* Botón de Logout *!/}
                 <Button variant="outline" onClick={handleLogout} className="w-40">
                     Logout
                 </Button>
-            </div>
+            </div>º
         </div>
-    );
+    );*/
+
+    return (
+        <div className="w-[50vw] m-auto flex-1 lg:max-w-2xl overflow-y-scroll h-full pb-10 px-4">
+            <form className="space-y-8">
+                <div>
+                    <h3 className="text-lg font-medium">Hi, Josep!</h3>
+                    <p className="text-sm text-muted-foreground">
+                        This is how others will see you on the site.
+                    </p>
+                </div>
+                <div className={"flex flex-row gap-16 w-full"}>
+                    <div className={"flex flex-col space-y-2"}>
+                        <Avatar className="w-28 h-28">
+                            <AvatarImage src={avatarUrl || "default-avatar.png"} alt="User Avatar"/>
+                            <AvatarFallback>{username ? username[0].toUpperCase() : 'U'}</AvatarFallback>
+                        </Avatar>
+                        <Button variant={"ghost"}>Upload image</Button>
+                    </div>
+                    <div className="space-y-2 w-full">
+                        <Label htmlFor="username">Username</Label>
+                        <Input id="username" defaultValue="shadcn"/>
+                    </div>
+                </div>
+
+                <div className={"flex flex-row gap-6"}>
+                    <div className="space-y-2 w-full">
+                        <Label htmlFor="name">Name</Label>
+                        <Input id="name" defaultValue="shadcn"/>
+                    </div>
+                    <div className="space-y-2 w-full">
+                        <Label htmlFor="surname">Surname</Label>
+                        <Input id="surname" defaultValue="shadcn"/>
+                    </div>
+                </div>
+
+                <div className="space-y-2 w-full">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" value={userInfo.email}/>
+                </div>
+                <div className="space-y-2">
+                    <Label htmlFor="bio">Bio</Label>
+                    <Textarea
+                        id="bio"
+                        defaultValue="I own a computer."
+                        className="resize-none"
+                    />
+                    <p className="text-sm text-muted-foreground">
+                        You can @mention other users and organizations to link to them.
+                    </p>
+                </div>
+                <div className={"flex flex-row gap-4"}>
+                    <Button type="submit">Change password</Button>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant="outline">Change password</Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px]">
+                            <DialogHeader>
+                                <DialogTitle>Create new password</DialogTitle>
+                                <DialogDescription>
+                                    Input new password. Click save when you're done.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <div className="grid gap-4 py-4">
+                                <div className="space-y-2 w-full">
+                                    <Label htmlFor="password">Current password</Label>
+                                    <Input id="password" placeholder={"Your current password"}/>
+                                </div>
+                                <div className="space-y-2 w-full">
+                                    <PasswordInput/>
+                                </div>
+                            </div>
+                            <DialogFooter>
+                                <Button type="submit">Change password</Button>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
+                </div>
+            </form>
+        </div>
+    )
 };
 
 export default ProfilePage;
